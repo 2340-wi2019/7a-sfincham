d67a361b0a0b8213af3a18e0f79ebcd0d8cf83a3
sfincham <58490391+sfincham@users.noreply.github.com>
2020-01-30 00:19:27 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Assignment 07</title>
+    <meta charset="utf-8">
+      
+    
+</head>
+
+<body>
+<!-- Scott Fincham
+assignment07
+INFO2340.WW
+Thoendel
+Winter 2019
+-->
+
+<?php
+     $food = "chili";
+     $rating = "3";
+     $place = "Disney World";
+     $animal = "dolphin";
+    //define variables here
+?>
+
+<h1> Good morning, Nebraska!</h1>
+<div>Last night I ate <?php echo $food; ?>. </div>
+<div>While it was good, it wasn't perfect. I'd give it <?php echo $rating; ?> stars out of 4.</div>
+<div>Tomorrow I plan to go to <?php echo $place; ?>.</div>
+<div>My favorite animal is the <?php echo $animal; ?>.</div>
+
+</body>
+
+</html>
+
